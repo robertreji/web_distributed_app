8d06a6cce5328a56317c38702d9bf1b051cf34be
robert <robertreji2005@gmail.com>
2026-02-14 22:35:37 +0530

    setup react + tailwind sucess

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+return(
+  <div className='bg-black w-full h-screen flex items-center justify-center'>
+    <p className="text-white">hlooo</p>
+  </div>
+)
+}
+
+export default App
