4315c9092e8840a59b64e63fcd0a67284b2661ef
robert <robertreji2005@gmail.com>
2026-02-17 01:52:52 +0530

    test commit

--- src/components/Patients.jsx
+++ src/components/Patients.jsx
@@ -4,7 +4,7 @@ function Patients({P_name,age,ward,bed_no,allergy}) {
     <div className="bg-white   shadow-[0_0px_10px_rgba(0,0,0,0.1)] relative w-[90%] h-32 rounded-2xl  flex pl-4 pt-4 pb-2 ">
       <div className="flex-1 flex gap-1 flex-col ">
         <p className="text-2xl font-bold font-serif" >{P_name}</p>
-        <p className="text-gray-500"> age :{age} ward : {ward}</p>
+        <p className="text-gray-400"> age :{age} ward : {ward}</p>
         <p className="flex  gap-2"><p className="text-gray-500">Bed :</p> <p>{bed_no}</p></p>
       </div>
       {
